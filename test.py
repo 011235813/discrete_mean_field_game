b8ce227708830db9a4607732c655fa79ff5676c4
011235813 <yjiachen@gmail.com>
2017-04-06 03:11:38 -0400

    Initial commit

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mfg_ac
+import time
 
 ac = mfg_ac.actor_critic()
 
@@ -21,7 +22,7 @@ def test_action():
     print("pi")
     print(pi)
     print("Sum of pi", np.sum(pi))
-    
+
     P = ac.sample_action(pi)
     
     print("P matrix")
@@ -34,6 +35,17 @@ def test_action():
     print(pi_next)
     print("Row sum", np.sum(pi_next))
 
+def time_action(n):
+    ac.init_pi0(r'C:\Users\Jiachen\Documents\Projects\Python\RL\MFG\data_train_reordered')
+    ac.num_start_samples = ac.mat_pi0.shape[0]
+    idx_row = ac.num_start_samples - 1
+    pi = ac.mat_pi0[idx_row, :]
+    t_start = time.time()
+    for j in range(n):
+        P = ac.sample_action(pi)
+    t_end = time.time()
+    print("Time taken for %d runs of sample_action" % n, (t_end - t_start))
+    
 
 def test_cost():
     ac = mfg_ac.actor_critic()
@@ -47,9 +59,26 @@ def test_cost():
 
     print("Reward", reward)
 
-def test_value():
-    ac = mfg_ac.actor_critic(d=3)
+def time_cost(n):
+    ac = mfg_ac.actor_critic()
+    # P = np.array([[1,3,3,4],[4,5,6,6],[7,8,9,10],[1,2,3,4]])
+    P = np.ones([47,47])
+    # pi = np.array([0.1,0.2,0.6,0.1])
+    pi = np.zeros(47)
+    pi[0] = 1
+    t_start = time.time()
+    for j in range(n):
+        reward = ac.calc_cost(P, pi, 47)
+    t_end = time.time()
+    print("Time taken for %d runs of calc_cost" % n, (t_end - t_start))
+    print("Reward", reward)
 
+    
+def test_value():
+    d = 3
+    ac = mfg_ac.actor_critic(d=d)
+    num_features = int((d+1)*d/2 + d + 1)
+    ac.w = np.ones(num_features)
     print("weights")
     print(ac.w)
 
@@ -57,10 +86,24 @@ def test_value():
     print("pi")
     print(pi)
 
-    # value = ac.calc_value(pi)
-    vec_features = ac.calc_features(pi)
-    value = vec_features.dot(ac.w)
+    value = ac.calc_value(pi)
+    # vec_features = ac.calc_features(pi)
+    # value = vec_features.dot(ac.w)
     print("value", value)
+
+def time_value(n):
+    d = 47
+    ac = mfg_ac.actor_critic(d=d)
+    num_features = int((d+1)*d/2 + d + 1)
+    ac.w = np.ones(num_features)
+    pi = np.random.rand(d)
+
+    t_start = time.time()
+    for j in range(n):
+        value = ac.calc_value(pi)
+    t_end = time.time()
+    print("Time taken for %d runs of calc_value" % n, (t_end - t_start))
+    print("Value", value)
 
 
 def test_gradient():
