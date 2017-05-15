2fc29c05474816addd73470657d2e6609b3ea2bd
011235813 <yjiachen@gmail.com>
2017-05-15 00:33:56 -0400

    Changed values

--- plots.py
+++ plots.py
@@ -6,8 +6,8 @@ from matplotlib.backends.backend_pdf import PdfPages
 #plt.rcParams.update({'font.size': 12})
 
 N = 2
-mfg = (0.0147, 0.0222)
-mfg_std = (0.009, 0.005)
+mfg = (0.00267, 0.00429) #(0.0147, 0.0222)
+mfg_std = (0.00162, 0.0019) #(0.009, 0.005)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.25       # the width of the bars
@@ -17,8 +17,8 @@ rects1 = ax.bar(ind, mfg, width, color='b', yerr=mfg_std)
 
 ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
-var = (0.0271, 0.0292)
-var_std = (0.0156, 0.0115)
+var = (0.00359, 0.00456) #(0.0271, 0.0292)
+var_std = (0.00101, 0.00104) #(0.0156, 0.0115)
 rects2 = ax.bar(ind + width, var, width, color='r', yerr=var_std)
 
 start, end = ax.get_ylim()
@@ -26,8 +26,8 @@ step = (end-start)/5
 ax.yaxis.set_ticks(np.arange(start, end+step, step))
 
 # add some text for labels, title and axes ticks
-ax.set_ylabel('L1 norm of difference')
-ax.set_title('Test error averaged over 11 days')
+ax.set_ylabel('Jensen-Shannon divergence')
+ax.set_title('Test error averaged over 10 days')
 ax.set_xticks(ind + width / 2)
 ax.set_xticklabels(('Final distribution', 'Entire trajectory (averaged)'))
 
@@ -38,6 +38,6 @@ for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
 ax.legend((rects1[0], rects2[0]), ('MFG', 'VAR'), bbox_to_anchor=(0.7,1), prop={'size':14})
 
 #plt.show()
-pp = PdfPages('plots/perf_mfg_var_m5d9.pdf')
+pp = PdfPages('plots/perf_mfg_t9p99_s0p02_alpha2e4_var_lag22.pdf')
 pp.savefig(fig)
 pp.close()
