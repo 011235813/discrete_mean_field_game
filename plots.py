f856e85d2dcdab8bc96058208beacfa9c463de1a
011235813 <yjiachen@gmail.com>
2017-05-19 16:19:09 -0400

    Added rnn

--- plots.py
+++ plots.py
@@ -6,11 +6,11 @@ from matplotlib.backends.backend_pdf import PdfPages
 #plt.rcParams.update({'font.size': 12})
 
 N = 2
-mfg = (0.00267, 0.00429) #(0.0147, 0.0222)
-mfg_std = (0.00162, 0.0019) #(0.009, 0.005)
+mfg = (0.0028, 0.0045) #(0.00267, 0.00429) #(0.0147, 0.0222)
+mfg_std = (0.0014, 0.0018) #(0.00162, 0.0019) #(0.009, 0.005)
 
 ind = np.arange(N)  # the x locations for the groups
-width = 0.25       # the width of the bars
+width = 0.15       # the width of the bars
 
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind, mfg, width, color='b', yerr=mfg_std)
@@ -21,23 +21,28 @@ var = (0.00359, 0.00456) #(0.0271, 0.0292)
 var_std = (0.00101, 0.00104) #(0.0156, 0.0115)
 rects2 = ax.bar(ind + width, var, width, color='r', yerr=var_std)
 
+rnn = (0.613119, 0.594970)
+rnn_std = (0.007203, 0.008202)
+rects3 = ax.bar(ind + 2*width, rnn, width, color='m', yerr=rnn_std)
+
 start, end = ax.get_ylim()
 step = (end-start)/5
-ax.yaxis.set_ticks(np.arange(start, end+step, step))
+ax.yaxis.set_ticks(np.arange(start, end+step*0.9, step))
 
 # add some text for labels, title and axes ticks
-ax.set_ylabel('Jensen-Shannon divergence')
-ax.set_title('Test error averaged over 10 days')
+ax.set_ylabel('JSD (log scale)')
+ax.set_title('Average test error over 10 days')
 ax.set_xticks(ind + width / 2)
 ax.set_xticklabels(('Final distribution', 'Entire trajectory (averaged)'))
+ax.set_yscale('log')
 
 for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
              ax.get_xticklabels()):
     item.set_fontsize(14)
 
-ax.legend((rects1[0], rects2[0]), ('MFG', 'VAR'), bbox_to_anchor=(0.7,1), prop={'size':14})
+ax.legend((rects1[0], rects2[0], rects3[0]), ('MFG', 'VAR', 'RNN'), bbox_to_anchor=(0.65,1), prop={'size':14})
 
 #plt.show()
-pp = PdfPages('plots/perf_mfg_t9p99_s0p02_alpha2e4_var_lag22.pdf')
+pp = PdfPages('plots/chart_mfg_t8p86_s0p16_alpha12000_var_lag22_rnn.pdf')
 pp.savefig(fig)
 pp.close()
