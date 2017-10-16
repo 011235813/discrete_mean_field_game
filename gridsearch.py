e4546f5e7172532163db435cd74aa39b9ad68e39
011235813 <yjiachen@gmail.com>
2017-10-16 17:08:41 -0400

    gridsearch.py : now records avg reward on generated transitions

--- gridsearch.py
+++ gridsearch.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 
 with open('results/reward_gridsearch.csv', 'w') as f:
-    f.write('reg,n_fc3,n_fc4,reward_demo_avg_train,reward_demo_avg_val,theta\n')
+    f.write('reg,n_fc3,n_fc4,reward_demo_avg_train,reward_demo_avg_test,reward_gen_avg,theta\n')
 
 list_reg = ['dropout', 'l1l2', 'dropout_l1l2']
 list_nfc3 = range(4,10,2)
@@ -25,7 +25,7 @@ for reg in list_reg:
             # Evaluate reward
             # tf.reset_default_graph()
             # ac = ac_irl.AC_IRL(theta=final_theta, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4, saved_network='model_%s_%d_%d.ckpt' % (reg, n_fc3, n_fc4))
-            reward_demo_avg_train, reward_demo_avg_val = ac.test_reward_network()
+            reward_demo_avg_train, reward_demo_avg_test, reward_gen_avg = ac.test_reward_network()
             
             with open('results/reward_gridsearch.csv', 'a') as f:
-                f.write('%s,%d,%d,%f,%f,%f\n' % (reg, n_fc3, n_fc4, reward_demo_avg_train, reward_demo_avg_val, final_theta))
+                f.write('%s,%d,%d,%f,%f,%f,%f\n' % (reg, n_fc3, n_fc4, reward_demo_avg_train, reward_demo_avg_test, reward_gen_avg, final_theta))
