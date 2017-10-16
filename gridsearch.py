5076c5553e13e1f0d19f24d30df1430e59f46e05
011235813 <yjiachen@gmail.com>
2017-10-15 22:44:26 -0400

    gridsearch.py : initial commit

---
+++ gridsearch.py
@@ -0,0 +1,31 @@
+import ac_irl
+import tensorflow as tf
+
+
+with open('results/reward_gridsearch.csv', 'w') as f:
+    f.write('reg,n_fc3,n_fc4,reward_demo_avg_train,reward_demo_avg_val,theta\n')
+
+list_reg = ['dropout', 'l1l2', 'dropout_l1l2']
+list_nfc3 = range(4,10,2)
+list_nfc4 = range(4,10,2)
+
+for reg in list_reg:
+
+    for n_fc3 in list_nfc3:
+
+        for n_fc4 in list_nfc4:
+
+            print("---------- reg = %s | n_fc3 = %d | n_fc4 = %d ----------" % (reg, n_fc3, n_fc4))
+
+            # Train
+            tf.reset_default_graph()
+            ac = ac_irl.AC_IRL(theta=6.0, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4)
+            final_theta = ac.outerloop()
+            
+            # Evaluate reward
+            # tf.reset_default_graph()
+            # ac = ac_irl.AC_IRL(theta=final_theta, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4, saved_network='model_%s_%d_%d.ckpt' % (reg, n_fc3, n_fc4))
+            reward_demo_avg_train, reward_demo_avg_val = ac.test_reward_network()
+            
+            with open('results/reward_gridsearch.csv', 'a') as f:
+                f.write('%s,%d,%d,%f,%f,%f\n' % (reg, n_fc3, n_fc4, reward_demo_avg_train, reward_demo_avg_val, final_theta))
