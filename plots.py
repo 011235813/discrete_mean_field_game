3451a46d66bcbe50ee60395a0256798364347f1a
011235813 <yjiachen@gmail.com>
2017-10-17 12:14:31 -0400

    plots.py : added heatmap examples

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from pylab import flipud
 import pandas as pd
 
 #plt.rcParams.update({'font.size': 12})
@@ -67,43 +68,112 @@ def plot_reward_convergence(filename='reward_convergence.csv', outfile='plots_ir
 
 def test_histogram(normed=True):
     mu, sigma = 100, 15
-    # x = mu + sigma * np.random.randn(10000)
-    x = np.ones(100)
+    x = mu + sigma * np.random.randn(100)
+    # x = np.ones(100)
     mu, sigma = 85, 15
-    # y = mu + sigma * np.random.randn(10000)
-    y = np.ones(100) * 2
+    y = mu + sigma * np.random.randn(100)
+    # y = np.ones(100) * 2
     mu, sigma = 115, 15
-    # z = mu + sigma * np.random.randn(10000)
-    z = np.ones(100) * -1
+    z = mu + sigma * np.random.randn(100)
+    # z = np.ones(100) * -1
 
     fig = plt.figure(1)
     plt.subplot(311)
     n, bins, patches = plt.hist(x, 50, normed=normed, facecolor='g', alpha=0.75)
+    axes = plt.gca()
+    print(n)
+    print(bins)
+    # y_min, y_max = axes.get_ylim()
+    # print(y_min, y_max)
     plt.xlabel('Smarts')
     plt.ylabel('Probability')
     plt.title('Histogram of IQ')
     plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
     # plt.axis([40, 160, 0, 0.03])
+    # plt.axis([40, 160, y_min, y_max])
 
     plt.subplot(312)
     n, bins, patches = plt.hist(y, 50, normed=normed, facecolor='r', alpha=0.75)
+    axes = plt.gca()
+    y_min, y_max = axes.get_ylim()
+    print(y_min, y_max)
     plt.xlabel('Smarts')
     plt.ylabel('Probability')
     plt.title('Histogram of IQ')
     plt.text(60, .025, r'$\mu=85,\ \sigma=15$')
     # plt.axis([40, 160, 0, 0.03])
+    # plt.axis([40, 160, y_min, y_max])
 
     plt.subplot(313)
     n, bins, patches = plt.hist(z, 50, normed=normed, facecolor='b', alpha=0.75)
+    axes = plt.gca()
+    y_min, y_max = axes.get_ylim()
+    print(y_min, y_max)
     plt.xlabel('Smarts')
     plt.ylabel('Probability')
     plt.title('Histogram of IQ')
     plt.text(60, .025, r'$\mu=115,\ \sigma=15$')
     # plt.axis([40, 160, 0, 0.03])        
+    # plt.axis([40, 160, y_min, y_max])
 
     pp = PdfPages("test_histogram.pdf")
     pp.savefig(fig)
     pp.close()
 
-    # plt.grid(True)
-    # plt.show()    
+
+def test_heatmap():
+    # Generate some data that where each slice has a different range
+    # (The overall range is from 0 to 2)
+    arr1 = np.random.random((15,15))
+    arr2 = np.random.random((15,15))
+    data = np.stack([arr1, arr2], axis=0)
+    
+    # Plot each slice as an independent subplot
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    # for dat, ax in zip(data, axes.flat):
+    #     # The vmin and vmax arguments specify the color limits
+    #     im = ax.imshow(dat, cmap='hot', vmin=0, vmax=1)
+    ax1 = axes[0]
+    im = ax1.imshow(arr1, cmap='hot', vmin=0, vmax=1)
+    ax1.set_title('hello')
+
+    ax2 = axes[1]
+    im = ax2.imshow(arr2, cmap='hot', vmin=0, vmax=1)
+    ax2.set_title('bye')
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.3, 0.05, 0.4])
+    fig.colorbar(im, cax=cbar_ax)
+    
+    # Make an axis for the colorbar on the right side
+    # cax = fig.add_axes([0.9, 0.1, 0.03, 0.8]) # xmin, ymin, dx, dy
+    # fig.colorbar(im, cax=cax)
+    
+    pp = PdfPages("test_heatmap.pdf")
+    pp.savefig(fig)
+    pp.close()
+
+
+def test_colorbar():
+    # Builtin colourmap "seismic" has the blue-white-red
+    #   scale you want
+    a = np.zeros((16,16), dtype=np.float32)
+    a[0,0] = 1.0
+    b = np.ones((16,16), dtype=np.float32) * 0.5
+    b[0,0] = 1.0
+    
+    fig = plt.figure()
+    plt.subplot(121, aspect='equal')
+    # plt.pcolor(data, cmap=plt.cm.seismic, vmin=0, vmax=2)
+    plt.pcolor(flipud(a), cmap='hot', vmin=0, vmax=1)
+    plt.colorbar()
+
+    plt.subplot(122, aspect='equal')
+    plt.pcolor(flipud(b), cmap='hot', vmin=0, vmax=1)
+    plt.colorbar()
+
+    pp = PdfPages("test_colorbar.pdf")
+    pp.savefig(fig)
+    pp.close()
+
+
