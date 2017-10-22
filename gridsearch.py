a698a8e9d977efd33c411cb2403fb74749d51cc2
011235813 <yjiachen@gmail.com>
2017-10-22 17:00:28 -0400

    gridsearch.py : updated default args

--- gridsearch.py
+++ gridsearch.py
@@ -2,7 +2,7 @@ import ac_irl
 import tensorflow as tf
 
 
-with open('results/reward_gridsearch.csv', 'w') as f:
+with open('results/reward_gridsearch_10_22.csv', 'w') as f:
     f.write('reg,n_fc3,n_fc4,reward_demo_avg_train,reward_demo_avg_test,reward_gen_avg,theta\n')
 
 list_reg = ['dropout', 'l1l2', 'dropout_l1l2']
@@ -19,7 +19,7 @@ for reg in list_reg:
 
             # Train
             tf.reset_default_graph()
-            ac = ac_irl.AC_IRL(theta=6.0, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4)
+            ac = ac_irl.AC_IRL(theta=6.5, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4)
             final_theta = ac.outerloop()
             
             # Evaluate reward
@@ -27,5 +27,5 @@ for reg in list_reg:
             # ac = ac_irl.AC_IRL(theta=final_theta, reg=reg, n_fc3=n_fc3, n_fc4=n_fc4, saved_network='model_%s_%d_%d.ckpt' % (reg, n_fc3, n_fc4))
             reward_demo_avg_train, reward_demo_avg_test, reward_gen_avg = ac.test_reward_network()
             
-            with open('results/reward_gridsearch.csv', 'a') as f:
+            with open('results/reward_gridsearch_10_22.csv', 'a') as f:
                 f.write('%s,%d,%d,%f,%f,%f,%f\n' % (reg, n_fc3, n_fc4, reward_demo_avg_train, reward_demo_avg_test, reward_gen_avg, final_theta))
